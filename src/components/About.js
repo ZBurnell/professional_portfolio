991727c630cf3651185a42800b3f18a27bd45f5a
Zack <zackburnell922@gmail.com>
2024-05-29 19:58:25 -0400

    small changes to build

--- src/components/About.js
+++ src/components/About.js
@@ -9,9 +9,9 @@ export default function About() {
             lets build something cool together!
           </h1>
           <p className="mb-8 leading-relaxed">
-            Hello! My name is Zack. I am a highly motivated and resourceful web developer with a passion for technology. As a recent graduate of a
-            rigorous coding boot camp I am eager to leverage all of the skills I have gained with past career
-            experiences to contribute effectively to innovative projects and grow within the field. A quick learner with
+            Highly motivated and resourceful web developer with a passion for technology. As a recent graduate of a
+            rigorous coding boot camp, I am eager to leverage all the skills gained from my past career
+            experiences to build innovative projects and grow within the field. Known as a quick learner with
             a collaborative mindset, seeking opportunities to apply creativity and problem-solving abilities to drive
             meaningful impact in a diverse work environment.
           </p>
